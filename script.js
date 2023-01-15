cca52e3c36b83bee0b53307800e8d91263a7d4c9
Eleonora Mattera <m.eleonora98@live.it>
2023-01-16 00:29:27 +0100

    css js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-const functionscoll = function () {
-  const scroll = document.getElementsByTagName("nav");
-  scroll.style.backgroundColor = "white";
+const list = document.querySelector(".quinto");
+const navb = document.querySelector(".nav");
+window.onscroll = () => {
+  if (window.scrollY > 400) {
+    navb.classList.add("nav-scroll");
+    list.classList.add("quinto-scroll");
+  } else {
+    navb.classList.remove("nav-scroll");
+    list.classList.remove("quinto-scroll");
+  }
 };
+
+let m = document.querySelectorAll("svg g[stroke-linecap='butt'] "); //seleziono tutte e m e le salvo in una variabile//
+
+let tempo = setInterval(() => {
+  let numeroAcaso = Math.floor(Math.random() * m.length);
+  let mAcaso = m[numeroAcaso];
+  mAcaso.style.transition = "0.2s";
+
+  if (mAcaso.getAttribute("opacity") === 1) {
+    mAcaso.setAttribute("opacity", "0");
+  } else {
+    mAcaso.setAttribute("opacity", "1");
+  }
+}, 50); //millisecondi//*/
